2573115baeca03458838f6c83a89c0268d37839a
kerrimurphy <107213992+kerrimurphy@users.noreply.github.com>
2022-11-14 20:52:12 +0000

    Level1-6

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,22 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+select * from customers
+where lastname = 'Norby' or firstname = 'Norby'
+
+select * from reservations
+where customerid = 64
+
+SELECT * FROM Reservations
+INNER JOIN Customers
+ON Reservations.CustomerId = Customers.CustomerId
+WHERE Customers.FirstName = 'Norby'
+
+SELECT * FROM Customers
+INNER JOIN Reservations
+ON Customers.CustomerId = Reservations.CustomerID
+WHERE Customers.FirstName = 'Norby'
+
+DELETE FROM Reservations
+where reservationid = 2000
